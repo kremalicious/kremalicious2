4344422be72b4372084da95409b19771315584fb
Matthias Kretschmann <desk@kremalicious.com>
2012-04-13 01:08:37 +0200

    put in the css basics from the prototype

--- html/kremalicious2/index.php
+++ html/kremalicious2/index.php
@@ -1,9 +1,6 @@
 <?php get_header(); ?>
     <div id="content">
       <div id="main" role="main">
-        <div class="page-header">
-          <h1><?php _e('Latest Posts', 'roots');?></h1>
-        </div>
         <?php get_template_part('loop', 'index'); ?>
       </div><!-- /#main -->
     </div><!-- /#content -->
