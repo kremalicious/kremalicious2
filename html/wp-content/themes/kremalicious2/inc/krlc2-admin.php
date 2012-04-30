611ba09400abd3d8a1ee2c19f8bdb57a3af903af
Matthias Kretschmann <desk@kremalicious.com>
2012-04-30 11:11:58 +0200

    disable incoming links widget

--- html/wp-content/themes/kremalicious2/inc/krlc2-admin.php
+++ html/wp-content/themes/kremalicious2/inc/krlc2-admin.php
@@ -2,7 +2,7 @@
 
 // http://www.deluxeblogtips.com/2011/01/remove-dashboard-widgets-in-wordpress.html
 function roots_remove_dashboard_widgets() {
-  //remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+  remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
   remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
   remove_meta_box('dashboard_primary', 'dashboard', 'normal');
   remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
