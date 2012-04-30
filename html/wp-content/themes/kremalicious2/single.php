79badff22c2b27543aab922bbcad7528617459e9
Matthias Kretschmann <desk@kremalicious.com>
2012-04-30 11:05:54 +0200

    dev warning in single view too

--- html/wp-content/themes/kremalicious2/single.php
+++ html/wp-content/themes/kremalicious2/single.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 	<section role="main">
+		<div id="devWarning" class="row divider-bottom">
+			<p class="alert alert-block col6">
+				<i class="icon-warning-sign icon-large"></i>This site is currently undergoing heavy restructuring &amp; maintenance. Please excuse if some parts of it behave somehow funky for you. Oh, and welcome to the new site!
+			</p>
+		</div>
 		<?php get_template_part('loop', 'single'); ?>
 	</section><!-- /#main -->
 	
