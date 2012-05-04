a965f990d9c3b52a8f84341fa8fe9ec4034806b1
Matthias Kretschmann <desk@kremalicious.com>
2012-05-04 18:22:53 +0200

    show follow count on twitter button, only when socialised

--- html/wp-content/themes/kremalicious2/sidebar.php
+++ html/wp-content/themes/kremalicious2/sidebar.php
@@ -73,7 +73,7 @@
 		</p>
 		<div id="tweetsWrap" class="divider-top">
 			<div id="tweets" class="dimmed"></div>
-			<a class="btn socialite twitter follow" href="https://twitter.com/kremalicious" data-show-count="false"><i class="icon-twitter-alt"></i> Follow @kremalicious</a>
+			<a class="btn socialite twitter follow" href="https://twitter.com/kremalicious"><i class="icon-twitter-alt"></i> Follow @kremalicious</a>
 		</div>
 		
 
