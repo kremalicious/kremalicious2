60a68f94e1ec64558eff1f226677187c10b8a51d
Matthias Kretschmann <desk@kremalicious.com>
2012-05-05 22:54:36 +0200

    never used

--- html/wp-content/themes/kremalicious2/page-goodies.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Template Name: Goodies Page
-*/
-get_header(); ?>
-	<section role="main" class="row">
-        <?php get_template_part('loop', 'goodies'); ?>
-	</section>
-<?php get_footer(); ?>
