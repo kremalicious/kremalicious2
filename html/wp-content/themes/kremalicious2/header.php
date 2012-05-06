c8f250b6505ff91ed974fc17b1f7035310b066d0
Matthias Kretschmann <desk@kremalicious.com>
2012-05-06 05:28:28 +0200

    async typekit loading and initial body text states

--- html/wp-content/themes/kremalicious2/header.php
+++ html/wp-content/themes/kremalicious2/header.php
@@ -27,8 +27,32 @@
 	
 	<meta http-equiv="cleartype" content="on">
 	
-	<script type="text/javascript" src="http://use.typekit.com/msu4qap.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script>
+	TypekitConfig = {
+		kitId: 'msu4qap',
+		scriptTimeout: 3000
+	};
+	(function() {
+		var h = document.getElementsByTagName('html')[0];
+		h.className += ' wf-loading';
+		var t = setTimeout(function() {
+		  h.className = h.className.replace(/(\s|^)wf-loading(\s|$)/g, '');
+		  h.className += ' wf-inactive';
+		}, TypekitConfig.scriptTimeout);
+		var tk = document.createElement('script');
+		tk.src = '//use.typekit.com/' + TypekitConfig.kitId + '.js';
+		tk.type = 'text/javascript';
+		tk.async = 'true';
+		tk.onload = tk.onreadystatechange = function() {
+		  var rs = this.readyState;
+		  if (rs && rs != 'complete' && rs != 'loaded') return;
+		  clearTimeout(t);
+		  try { Typekit.load(TypekitConfig); } catch (e) {}
+		};
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(tk, s);
+	})();
+    </script>
 	
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/modernizr-2.5.3.min.js"></script>
 
