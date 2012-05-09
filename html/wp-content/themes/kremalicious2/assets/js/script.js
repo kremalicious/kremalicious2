0db20c00051a6896fc4f980af8606e500345bd2a
Matthias Kretschmann <desk@kremalicious.com>
2012-05-10 00:08:10 +0200

    better ios scale fix implementation, remove initial-scale to make script work

--- html/wp-content/themes/kremalicious2/assets/js/script.js
+++ html/wp-content/themes/kremalicious2/assets/js/script.js
@@ -19,6 +19,7 @@ $(ASAP = function(){
 		photoGrid.init();
 	}
 	interface.init();
+	iosScaleFix.init();
 	
 });
 
@@ -160,65 +161,6 @@ var interface = {
 		
 	},
 	
-	iosScaleFix: function() {
-		/*! A fix for the iOS orientationchange zoom bug.
-		 Script by @scottjehl, rebound by @wilto.
-		 https://github.com/scottjehl/iOS-Orientationchange-Fix
-		 MIT License.
-		*/
-		(function(w){
-		
-			// This fix addresses an iOS bug, so return early if the UA claims it's something else.
-			if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
-				return;
-			}
-		
-		    var doc = w.document;
-		
-		    if( !doc.querySelector ){ return; }
-		
-		    var meta = doc.querySelector( "meta[name=viewport]" ),
-		        initialContent = meta && meta.getAttribute( "content" ),
-		        disabledZoom = initialContent + ",maximum-scale=1",
-		        enabledZoom = initialContent + ",maximum-scale=10",
-		        enabled = true,
-				x, y, z, aig;
-		
-		    if( !meta ){ return; }
-		
-		    function restoreZoom(){
-		        meta.setAttribute( "content", enabledZoom );
-		        enabled = true;
-		    }
-		
-		    function disableZoom(){
-		        meta.setAttribute( "content", disabledZoom );
-		        enabled = false;
-		    }
-		
-		    function checkTilt( e ){
-				aig = e.accelerationIncludingGravity;
-				x = Math.abs( aig.x );
-				y = Math.abs( aig.y );
-				z = Math.abs( aig.z );
-		
-				// If portrait orientation and in one of the danger zones
-		        if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
-					if( enabled ){
-						disableZoom();
-					}        	
-		        }
-				else if( !enabled ){
-					restoreZoom();
-		        }
-		    }
-
-			w.addEventListener( "orientationchange", restoreZoom, false );
-			w.addEventListener( "devicemotion", checkTilt, false );
-
-		})( this );
-	},
-	
 	init: function(){
 		this.commentShowup();
 		this.bannerHomeLink();
@@ -226,9 +168,72 @@ var interface = {
 		this.rememberCloseAlerts();
 		$('#respond label').inFieldLabels();
 		$('#topicmenu .dropdown-toggle').dropdown();
-		this.iosScaleFix();
 	}
 		
+}
+
+var iosScaleFix = {
+	/*! A fix for the iOS orientationchange zoom bug.
+	 Script by @scottjehl, rebound by @wilto.
+	 https://github.com/scottjehl/iOS-Orientationchange-Fix
+	 MIT License.
+	*/
+	leFix: function(w){
+	
+		// This fix addresses an iOS bug, so return early if the UA claims it's something else.
+		if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
+			return;
+		}
+	
+	    var doc = w.document;
+	
+	    if( !doc.querySelector ){ return; }
+	
+	    var meta = doc.querySelector( "meta[name=viewport]" ),
+	        initialContent = meta && meta.getAttribute( "content" ),
+	        disabledZoom = initialContent + ",maximum-scale=1",
+	        enabledZoom = initialContent + ",maximum-scale=10",
+	        enabled = true,
+			x, y, z, aig;
+	
+	    if( !meta ){ return; }
+	
+	    function restoreZoom(){
+	        meta.setAttribute( "content", enabledZoom );
+	        enabled = true;
+	    }
+	
+	    function disableZoom(){
+	        meta.setAttribute( "content", disabledZoom );
+	        enabled = false;
+	    }
+	
+	    function checkTilt( e ){
+			aig = e.accelerationIncludingGravity;
+			x = Math.abs( aig.x );
+			y = Math.abs( aig.y );
+			z = Math.abs( aig.z );
+	
+			// If portrait orientation and in one of the danger zones
+	        if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+				if( enabled ){
+					disableZoom();
+				}        	
+	        }
+			else if( !enabled ){
+				restoreZoom();
+	        }
+	    }
+
+		w.addEventListener( "orientationchange", restoreZoom, false );
+		w.addEventListener( "devicemotion", checkTilt, false );
+
+	},
+	
+	init: function(){
+		this.leFix();
+		
+	}
 }
 
 var siteEffects = {
