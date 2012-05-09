6cee79ed25c3995f3f0f8a85499c349a6c2e4f27
Matthias Kretschmann <desk@kremalicious.com>
2012-05-09 23:32:27 +0200

    new iOS scale fix

--- html/wp-content/themes/kremalicious2/assets/js/script.js
+++ html/wp-content/themes/kremalicious2/assets/js/script.js
@@ -160,6 +160,65 @@ var interface = {
 		
 	},
 	
+	iosScaleFix: function() {
+		/*! A fix for the iOS orientationchange zoom bug.
+		 Script by @scottjehl, rebound by @wilto.
+		 https://github.com/scottjehl/iOS-Orientationchange-Fix
+		 MIT License.
+		*/
+		(function(w){
+		
+			// This fix addresses an iOS bug, so return early if the UA claims it's something else.
+			if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
+				return;
+			}
+		
+		    var doc = w.document;
+		
+		    if( !doc.querySelector ){ return; }
+		
+		    var meta = doc.querySelector( "meta[name=viewport]" ),
+		        initialContent = meta && meta.getAttribute( "content" ),
+		        disabledZoom = initialContent + ",maximum-scale=1",
+		        enabledZoom = initialContent + ",maximum-scale=10",
+		        enabled = true,
+				x, y, z, aig;
+		
+		    if( !meta ){ return; }
+		
+		    function restoreZoom(){
+		        meta.setAttribute( "content", enabledZoom );
+		        enabled = true;
+		    }
+		
+		    function disableZoom(){
+		        meta.setAttribute( "content", disabledZoom );
+		        enabled = false;
+		    }
+		
+		    function checkTilt( e ){
+				aig = e.accelerationIncludingGravity;
+				x = Math.abs( aig.x );
+				y = Math.abs( aig.y );
+				z = Math.abs( aig.z );
+		
+				// If portrait orientation and in one of the danger zones
+		        if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+					if( enabled ){
+						disableZoom();
+					}        	
+		        }
+				else if( !enabled ){
+					restoreZoom();
+		        }
+		    }
+
+			w.addEventListener( "orientationchange", restoreZoom, false );
+			w.addEventListener( "devicemotion", checkTilt, false );
+
+		})( this );
+	},
+	
 	init: function(){
 		this.commentShowup();
 		this.bannerHomeLink();
@@ -167,6 +226,7 @@ var interface = {
 		this.rememberCloseAlerts();
 		$('#respond label').inFieldLabels();
 		$('#topicmenu .dropdown-toggle').dropdown();
+		this.iosScaleFix();
 	}
 		
 }
