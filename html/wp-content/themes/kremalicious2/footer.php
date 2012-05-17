bac944e977f1af5c004ec8c6025a9d91d542d4f0
Matthias Kretschmann <desk@kremalicious.com>
2012-05-17 23:37:01 +0200

    performance test: async loading of modernizr & jquery

--- html/wp-content/themes/kremalicious2/footer.php
+++ html/wp-content/themes/kremalicious2/footer.php
@@ -33,8 +33,8 @@
 		
 	</div>
 	
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<script async src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script async src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
   	
   	<?php wp_footer(); ?>
 
