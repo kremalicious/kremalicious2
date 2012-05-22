67d856053fc2ab1b18eaf22e955da919f03ab322
Matthias Kretschmann <desk@kremalicious.com>
2012-05-22 09:01:49 +0200

    set some settings programmatically

--- html/wp-content/themes/kremalicious2/functions.php
+++ html/wp-content/themes/kremalicious2/functions.php
@@ -10,18 +10,43 @@ require_once locate_template('/inc/krlc2-htaccess.php');
 
 function kremalicious2_setup() {
 
-  // http://codex.wordpress.org/Post_Thumbnails
-  add_theme_support('post-thumbnails');
-  // set_post_thumbnail_size(150, 150, false);
-  add_image_size( 'featureImageBig', 960, 300, true );
-  add_image_size( 'featureImageStream', 540, 9999, true );
-  add_image_size( 'goodieImage', 650, 9999, true );
-  add_image_size( 'photoStream', 520, 9999 );
-  add_image_size( 'photoArchive', 300, 9999 );
-  add_image_size( 'photoBig', 960, 960 );
-
-  // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', array('link', 'image'));
+	// http://codex.wordpress.org/Post_Thumbnails
+	add_theme_support('post-thumbnails');
+	// set_post_thumbnail_size(150, 150, false);
+	add_image_size( 'featureImageBig', 960, 300, true );
+	add_image_size( 'featureImageStream', 540, 9999, true );
+	add_image_size( 'goodieImage', 650, 9999, true );
+	add_image_size( 'photoStream', 520, 9999 );
+	add_image_size( 'photoArchive', 300, 9999 );
+	add_image_size( 'photoBig', 960, 960 );
+	
+	// http://codex.wordpress.org/Post_Formats
+	add_theme_support('post-formats', array('link', 'image'));
+	
+	// Default Settings
+	////////////////////////////////////////
+	
+	// Reading
+	update_option('posts_per_page', 15);
+	update_option('posts_per_rss', 100);
+	
+	// Media
+	update_option('medium_size_w', 540);
+	update_option('medium_size_h', 540);
+	update_option('large_size_w', 960);
+	update_option('large_size_h', 960);
+	update_option('upload_path', 'media');
+	update_option('uploads_use_yearmonth_folders', 0);
+	
+	// Discussion
+	update_option('default_ping_status', 'closed');
+	update_option('default_pingback_flag', 1);
+	update_option('thread_comments', 1);
+	update_option('thread_comments_depth', 2);
+	
+	// Permalinks
+	update_option('permalink_structure', '/%postname%/');
+	update_option('category_base', 'topics');
 
 }
 add_action('after_setup_theme', 'kremalicious2_setup');
