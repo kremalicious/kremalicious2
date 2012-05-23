5f4fe32367a1ecd656bfac7c8c047ea50b4b58fa
Matthias Kretschmann <desk@kremalicious.com>
2012-05-23 22:31:54 +0200

    cacebusting: autoversioning function & htaccess rewrite from h5bp, use it for main css file

--- html/wp-content/themes/kremalicious2/functions.php
+++ html/wp-content/themes/kremalicious2/functions.php
@@ -51,6 +51,22 @@ function kremalicious2_setup() {
 }
 add_action('after_setup_theme', 'kremalicious2_setup');
 
+// Autoversioning of css and js files
+// http://derek.io/blog/2009/auto-versioning-javascript-and-css-files/
+/**
+ *  Given a file, i.e. /css/base.css, replaces it with a string containing the
+ *  file's mtime, i.e. /css/base.1221534296.css.
+ *  
+ *  @param $file  The file to be loaded.  Must be an absolute path (i.e.
+ *                starting with slash).
+ */
+function auto_version($file) {
+  if(strpos($file, '/') !== 0 || !file_exists($_SERVER['DOCUMENT_ROOT'] . $file))
+    return $file;
+
+  $mtime = filemtime($_SERVER['DOCUMENT_ROOT'] . $file);
+  return preg_replace('{\\.([^./]+)$}', ".$mtime.\$1", $file);
+}
 
 // add to virtual robots.txt
 function krlc2_robots() {
--- html/wp-content/themes/kremalicious2/header.php
+++ html/wp-content/themes/kremalicious2/header.php
@@ -51,7 +51,7 @@
 	
 	<meta http-equiv="cleartype" content="on">
 	
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/kremalicious2.min.css">
+	<link rel="stylesheet" href="<?php echo auto_version('/wp-content/themes/kremalicious2/assets/css/kremalicious2.min.css'); ?>">
 	
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/modernizr-2.5.3.min.js"></script>
 	<script src="//use.typekit.com/msu4qap.js"></script>
