ee040deeb6c0b886c7e0ebaeeb9daa11aa25a326
Matthias Kretschmann <desk@kremalicious.com>
2012-05-26 10:52:23 +0200

    I should totally link those github repos

--- html/wp-content/themes/kremalicious2/loop-page-about.php
+++ html/wp-content/themes/kremalicious2/loop-page-about.php
@@ -33,10 +33,10 @@
 				<h3>Latest Code</h3>
 				<p><a href="https://github.com/kremalicious" title="Github Profile" class="btn btn-tag icon-github-alt">Github</a></p>
 			</div>
-			<p class="col1"><a href="">kremalicious2</a> <small class="dimmed">WordPress theme &amp; site files powering kremalicious.com</small></p>
-			<p class="col1"><a href="">wp-icons-template</a> <small class="dimmed">Admin icons template &amp; functions for WordPress</small></p>
-			<p class="col1"><a href="">csspaperstack</a> <small class="dimmed">Pure CSS3 based solution for creating the illusion of stacked papers</small></p>
-			<p class="col1"><a href="">glassfruit</a> <small class="dimmed">Growl notification style inspired by displays of a particular fruit company</small></p>
+			<p class="col1"><a href="https://github.com/kremalicious/kremalicious2">kremalicious2</a> <small class="dimmed">WordPress theme &amp; site files powering kremalicious.com</small></p>
+			<p class="col1"><a href="https://github.com/kremalicious/wp-icons-template">wp-icons-template</a> <small class="dimmed">Admin icons template &amp; functions for WordPress</small></p>
+			<p class="col1"><a href="https://github.com/kremalicious/csspaperstack">csspaperstack</a> <small class="dimmed">Pure CSS3 based solution for creating the illusion of stacked papers</small></p>
+			<p class="col1"><a href="https://github.com/kremalicious/glassfruit">glassfruit</a> <small class="dimmed">Growl notification style inspired by displays of a particular fruit company</small></p>
 		</div>
 		<div class="row">
 			<div class="col2">
