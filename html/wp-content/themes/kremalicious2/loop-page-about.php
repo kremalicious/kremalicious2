157da960c27d029218b958055a97b41b8bce9de4
Matthias Kretschmann <desk@kremalicious.com>
2012-05-26 07:44:17 +0200

    more about page

--- html/wp-content/themes/kremalicious2/loop-page-about.php
+++ html/wp-content/themes/kremalicious2/loop-page-about.php
@@ -21,21 +21,6 @@
 	</section>
 
 <?php endwhile; /* End loop */ ?>
-
-	<section class="row" id="work">
-		<div class="col2">
-			<h2><i class="icon-github-alt"></i>Code</h2>
-			<img src="http://placekitten.com/315/200" width="315" height="200" />
-		</div>
-		<div class="col2">
-			<h2>Client Work</h2>
-			<img src="http://placekitten.com/315/200" width="315" height="200" />
-		</div>
-		<div class="col2">
-			<h2>Dribbble</h2>
-			<img src="http://placekitten.com/315/200" width="315" height="200" />
-		</div>
-	</div>
 	
 	<section class="row" id="contact">
 		<div class="col2">
@@ -57,6 +42,43 @@
 			* Github
 		</div>
 	</section>
+	
+	<section id="work">
+		<div class="row">
+			<div class="col2">
+				<h3>Latest Code</h3>
+				<p><a href="#" class="btn btn-tag icon-github-alt">Github</a></p>
+			</div>
+			<p class="col1"><a href="">kremalicious2</a> <small class="dimmed">WordPress theme &amp; site files powering kremalicious.com</small></p>
+			<p class="col1"><a href="">wp-icons-template</a> <small class="dimmed">Admin icons template &amp; functions for WordPress</small></p>
+			<p class="col1"><a href="">csspaperstack</a> <small class="dimmed">Pure CSS3 based solution for creating the illusion of stacked papers</small></p>
+			<p class="col1"><a href="">glassfruit</a> <small class="dimmed">Growl notification style inspired by displays of a particular fruit company</small></p>
+		</div>
+		<div class="row">
+			<div class="col2">
+				<h3>Latest Client Work</h3>
+				<p><a href="http://matthiaskretschmann.com" class="btn btn-tag">Portfolio</a></p>
+			</div>
+			<div class="col2">
+				<img src="http://matthiaskretschmann.com/folio/Portfolio-Mr-Reader-1.jpg" width="400" height="300" />
+			</div>
+			<div class="col2">
+				<img src="http://matthiaskretschmann.com/folio/Portfolio-IPP-Halle-1.png" width="400" height="300" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col2">
+				<h3>Latest Dribbble</h3>
+				<p><a href="#" class="btn btn-tag">Dribbble</a></p>
+			</div>
+			<div class="col2">
+				<img src="http://placekitten.com/315/200" width="315" height="200" />
+			</div>
+			<div class="col2">
+				<img src="http://placekitten.com/315/200" width="315" height="200" />
+			</div>
+		</div>
+	</div>
 
 	Credits/Colophon
 
