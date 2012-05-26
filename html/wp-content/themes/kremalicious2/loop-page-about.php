91617dbc19a92e6b311f40c71f1cf9a7c122c2ff
Matthias Kretschmann <desk@kremalicious.com>
2012-05-26 10:42:00 +0200

    publish state, a lot of stuff is still static

--- html/wp-content/themes/kremalicious2/loop-page-about.php
+++ html/wp-content/themes/kremalicious2/loop-page-about.php
@@ -3,10 +3,15 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <article <?php post_class() ?> id="aboutPage">
-
+	
+	<section class="row">
+		<div class="col6">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-kremalicious-g-profile.png" alt="logo-kremalicious-g-profile" width="940" height="180" />
+		</div>
+	</section>
+	
 	<section class="row" id="hello">
 		<div class="col2">
-			<h2>What’s this</h2>
 	
 			<p>This blog is where I post random stuff mostly about design, front-end development, photography.</p>
 			
@@ -14,40 +19,19 @@
 			
 			<?php krlc2_subscribe_buttons(); ?>
 		</div>
-		<div class="col4">
+		<div class="col4 entry-content">
 			<?php the_content(); ?>
 		</div>
 		
 	</section>
 
 <?php endwhile; /* End loop */ ?>
-	
-	<section class="row" id="contact">
-		<div class="col2">
-			<h2>Contact</h2>
-	
-			Twitter
-			Email desk@kremalicious.com
-		</div>
-		<div class="col4">
-			<h2>Where to find me</h2>
-	
-			* Portfolio
-			* Twitter
-			* Google+
-			* Dribbble
-			* Zerply
-			* 500px
-			* Flickr
-			* Github
-		</div>
-	</section>
-	
+
 	<section id="work">
 		<div class="row">
 			<div class="col2">
 				<h3>Latest Code</h3>
-				<p><a href="#" class="btn btn-tag icon-github-alt">Github</a></p>
+				<p><a href="https://github.com/kremalicious" title="Github Profile" class="btn btn-tag icon-github-alt">Github</a></p>
 			</div>
 			<p class="col1"><a href="">kremalicious2</a> <small class="dimmed">WordPress theme &amp; site files powering kremalicious.com</small></p>
 			<p class="col1"><a href="">wp-icons-template</a> <small class="dimmed">Admin icons template &amp; functions for WordPress</small></p>
@@ -57,29 +41,27 @@
 		<div class="row">
 			<div class="col2">
 				<h3>Latest Client Work</h3>
-				<p><a href="http://matthiaskretschmann.com" class="btn btn-tag">Portfolio</a></p>
+				<p><a href="http://matthiaskretschmann.com" class="btn btn-tag icon-star">Portfolio</a></p>
 			</div>
-			<div class="col2">
-				<img src="http://matthiaskretschmann.com/folio/Portfolio-Mr-Reader-1.jpg" width="400" height="300" />
-			</div>
-			<div class="col2">
+			<a href="http://matthiaskretschmann.com/" class="col2">
+				<img src="http://matthiaskretschmann.com/folio/Portfolio-Mr-Reader-Theme-1.png" width="400" height="300" />
+			</a>
+			<a href="http://matthiaskretschmann.com/" class="col2">
 				<img src="http://matthiaskretschmann.com/folio/Portfolio-IPP-Halle-1.png" width="400" height="300" />
-			</div>
+			</a>
 		</div>
 		<div class="row">
 			<div class="col2">
 				<h3>Latest Dribbble</h3>
 				<p><a href="#" class="btn btn-tag">Dribbble</a></p>
 			</div>
-			<div class="col2">
-				<img src="http://placekitten.com/315/200" width="315" height="200" />
-			</div>
-			<div class="col2">
-				<img src="http://placekitten.com/315/200" width="315" height="200" />
-			</div>
+			<a href="http://dribbble.com/shots/540014-kremalicious2" class="col2">
+				<img src="http://dribbble.com/system/assets/2525/73955/screenshots/540014/dribbble-kremalicious2.png" width="400" height="300" />
+			</a>
+			<a href="http://dribbble.com/shots/497613-GlassFruit-Growl-Style" class="col2">
+				<img src="http://dribbble.com/system/assets/2525/73955/screenshots/497613/dribbble_glassfruit.png" width="400" height="300" />
+			</a>
 		</div>
 	</div>
-
-	Credits/Colophon
 
 </article>
